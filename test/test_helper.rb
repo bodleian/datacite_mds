5f2bd56e4bd22124aa81f6bb48b72ad532d8ad21
Fred Heath <fred_h@bootstrap.me.uk>
2015-11-02 12:21:06 +0000

    trying to fix coveralls no-data issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'coveralls'
+Coveralls.wear!
+
 require 'datacite_mds'
 
 require 'minitest/autorun'
 require "minitest/pride"
 
-require 'coveralls'
-Coveralls.wear!
